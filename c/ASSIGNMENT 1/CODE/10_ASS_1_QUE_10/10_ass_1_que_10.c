80b6409033ba388aa04df68d342c47082df9d321
Darshan Pawar <140646683+DarshanPawar07@users.noreply.github.com>
2023-12-14 18:51:18 +0530

    Delete c/ASSIGNMENT 1/CODE directory

--- c/ASSIGNMENT 1/CODE/10_ASS_1_QUE_10/10_ass_1_que_10.c
+++
@@ -1,8 +0,0 @@
-#include<stdio.h>
-int main()
-{   int rad;
-    printf("Enter the 'r' radius of circle\n");
-    scanf("%d",&rad);
-    printf("The area of circle is %f having the radius %d  ",3.14*rad*rad,rad);
-    return 0;
-}
