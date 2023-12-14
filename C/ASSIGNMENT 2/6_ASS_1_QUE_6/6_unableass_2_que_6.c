4bcfda9f208e8bcb5b38d9d939fd0b09c345c897
Darshan Pawar <140646683+DarshanPawar07@users.noreply.github.com>
2023-12-14 19:01:37 +0530

    Delete C/ASSIGNMENT 2 directory

--- C/ASSIGNMENT 2/6_ASS_1_QUE_6/6_unableass_2_que_6.c
+++
@@ -1,9 +0,0 @@
-#include<stdio.h>/*ascii code waala que ,ye sir se seekhke kiya hai solve*/
-int main()
-   {char ch;
-    printf("Enter the character");
-    scanf("%c",&ch);
-
-    printf("ASCII code is %d",ch);
-    return 0;
-}
