511be11ac6fc1c04d22569dd9d048c98b619b119
Being_Errorless <140646683+DarshanPawar07@users.noreply.github.com>
2024-02-04 22:23:14 +0530

    Add files via upload

---
+++ C/ASSIGNMENT 2/Que_10.cpp
@@ -0,0 +1,23 @@
+
+
+        /* Que - . Define Circle class with radius as instance variable. Define two constructors in the 
+                   class - non parameterised and parameterised  */
+
+        #include<iostream>
+        using namespace std;
+
+        
+        class cuboid
+               {
+                    private:
+                           int l;
+                           int b;
+                           int h;
+                    
+                    
+
+               };
+         
+        
+
+     
